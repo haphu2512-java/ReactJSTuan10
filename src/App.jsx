1dc98b67065dd840f774637d4e513460e20aec11
haphu2512-java <haxuanphudeptrai123@gmail.com>
2025-04-26 16:15:00 +0700

    cau3_navbarMenu

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import TopHeader from './components/TopHeader';
 
 import Banner from './components/Banner'; // đường dẫn tùy project của bạn
 import LogoTitle from './components/LogoTitle';
+import { Nav } from 'react-bootstrap';
+import NavbarMenu from './components/NavbarMenu';
 
 function App() {
   return (
     <div>
       <TopHeader/>  {/* header bạn đã code */}
       <LogoTitle /> {/* thêm dòng này */}
+      <NavbarMenu />
+
       {/* <Banner />  */}
       {/* các thành phần khác */}
     </div>
